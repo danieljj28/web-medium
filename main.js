cc037a94adf592eb92e3e919b3ddb62286fd41fd
danieljj28 <danijj28@gmail.com>
2024-02-19 21:17:57 +0100

    Finalizado aside-sticky main-card y navbar fixed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const sectionContainer = document.querySelector(".section__container");
 const asideMain = document.querySelector(".aside__main");
+const main = document.querySelector(".main");
 
 /*El array con cada objeto card. */
 const cardsArticle = [{
@@ -43,6 +44,69 @@ const cardsArticle = [{
 /*El array con cada string del main__aside */
 const asideData = ["Programming", "Data Science", "Technology", "Self Improvement", "Writing", "Relationships", "Machine Learning", "Producticity", "Politics"]
 
+const cardsMain = [{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+},
+{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+},
+{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+},
+{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+},
+{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+},
+{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+},
+{
+    icon : "iconoNetflix.png",
+    tittle : "Riikka Livanainen",
+    p1 : "The secret life of people with high self-conrol (it's easier than you think)",
+    p2 : "Research suggest that people with high self-control are good at avoiding templation- ...",
+    date : "Jan 9, 2024",
+    timeRead : "8 min read",
+    category : "Self Control"
+}];
 /*Funcion que añade cada section al article__contiainer */
 const appendCard = () => {
     let htmlCode = "";
@@ -50,7 +114,7 @@ const appendCard = () => {
     for (const cardInformation of cardsArticle) {
         htmlCode += `<section class="article__card">
     <header class="article__card-header">
-        <div class = "card__img-container" style="background-image: url(${cardInformation.img}); background-size: contain; background-repeat: no-repeat;">
+        <div class = "card__img-container">
         </div>
         <p class = "card-header__paragraph">
             ${cardInformation.author}
@@ -79,9 +143,34 @@ const appendAsideData = () =>{
     }
     asideMain.innerHTML += htmlCode;
 }
+const appendMain = () =>{
+    let htmlCode = "";
+    for(const dataCard of cardsMain){
+        htmlCode += `<article class="main__card">
+        <header class="main__card-header">
+            <div class="main__card-header__container-img">
+            </div>
+            <p class="main__card-header__paragraph">${dataCard.tittle}</p>
+        </header>
+        <main class="main__card-main">
+            <p class="main__card-main__p1">${dataCard.p1}</p>
+            <p class="main__card-main__p2">${dataCard.p2}</p>
+        </main>
+        <footer class="main__card-footer">
+            <p>${dataCard.date}</p>
+            <p>- ${dataCard.timeRead}  -</p>
+            <p>${dataCard.category}</p>
+            <p><i class="fa-regular fa-bookmark"></i></p>
+        </footer>
+        <aside class="main__card-aside">
+            <img src="https://miro.medium.com/v2/resize:fill:400:268/1*H3DBMcaJgdDk4D16lggx5g.jpeg">
+        </aside>
+    </article>`
+    }
+    main.innerHTML += htmlCode;
+}
+
 
 appendCard();
 appendAsideData();
-
-
-
+appendMain();
